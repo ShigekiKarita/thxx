e71f2c645f5ca04cb7566d078913e49e6205216f
karita <shigekikarita@gmail.com>
2018-12-26 20:39:01 +0900

    fix travis

--- test/test_parser_main.cpp
+++ test/test_parser_main.cpp
@@ -1,30 +1,26 @@
 #include <thxx/parser.hpp>
-#include <iostream>
 
 using thxx::parser::ArgParser;
 
-struct Opt {
+struct Opt : ArgParser {
     std::int64_t batch_size = 32;
     std::vector<std::int64_t> units = {1, 2, 3};
     bool use_cuda = true;
     std::string expdir = "";
 
-    ArgParser parser;
-
-    Opt(ArgParser p) : parser(p) {
-        parser.required("--batch_size", batch_size, "batch size for training");
-        parser.add("--use_cuda", use_cuda, "flag to enable cuda");
-        parser.add("--units", units, "number of units");
-        parser.add("--expdir", expdir, "experiment directory");
-        if (parser.help_wanted) {
+    Opt(int argc, const char* const argv[]) : ArgParser(argc, argv) {
+        required("--batch_size", batch_size, "batch size for training");
+        add("--use_cuda", use_cuda, "flag to enable cuda");
+        add("--units", units, "number of units");
+        add("--expdir", expdir, "experiment directory");
+        if (help_wanted) {
             std::exit(0);
         }
-        parser.check();
+        check();
     }
 };
 
 int main(int argc, char *argv[]) {
-    Opt opt(ArgParser(argc, argv));
-    // AT_ASSERT(opt.batch_size == 4);
-    std::cout << opt.parser.to_json() << std::endl;
+    Opt opt(argc, argv);
+    std::cout << opt.to_json() << std::endl;
 }
