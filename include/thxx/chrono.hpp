2f08edbb3d0e02427fc25f11850998b6e13ce417
ShigekiKarita <shigekikarita@gmail.com>
2019-01-07 18:18:49 +0900

    refactor chrono

--- include/thxx/chrono.hpp
+++ include/thxx/chrono.hpp
@@ -7,11 +7,9 @@ namespace thxx::chrono {
     struct StopWatch {
         const C::high_resolution_clock::time_point start;
 
-        StopWatch() : start(now()) {}
+        using C::high_resolution_clock::now;
 
-        static C::high_resolution_clock::time_point now() {
-            return C::high_resolution_clock::now();
-        }
+        StopWatch() : start(now()) {}
 
         double elapsed() const {
             return 1e-9 * C::duration_cast<C::nanoseconds>(now() - this->start).count();
